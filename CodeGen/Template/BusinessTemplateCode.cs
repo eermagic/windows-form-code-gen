46c4d377be0b7b4712d216027b8290a468990afa
Mars <eermagic@yahoo.com.tw>
2022-06-30 23:20:20 +0800

    首次上傳

---
+++ CodeGen/Template/BusinessTemplateCode.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeGen.Template
+{
+    public partial class BusinessTemplate
+    {
+        public string TableName { get; set; }
+        public List<ColumnModel> Columns { get; set; }
+    }
+
+    public class ColumnModel
+    {
+        public string COLUMN_NAME { get; set; }
+        public string IS_NULLABLE { get; set; }
+        public string DATA_TYPE { get; set; }
+        public string CONSTRAINT_NAME { get; set; }
+    }
+}
